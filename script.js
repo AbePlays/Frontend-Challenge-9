4b29b63523686c080d66439c1cd9791294ce8ec7
Abe <abhi.rawat456@gmail.com>
2020-05-29 19:52:21 +0530

    JS added for nav bar

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+const main = document.querySelector(".main");
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const cross = document.querySelector(".cross");
+const links = document.querySelector(".links");
+console.log(links);
 
 toggle.addEventListener("click", () => {
   if (menu.style.display.localeCompare("none") === 0) {
     menu.style.display = "block";
     cross.style.display = "none";
+    main.style.background = "none";
+    links.classList.remove("open");
   } else {
     menu.style.display = "none";
     cross.style.display = "block";
+    main.style.background = "linear-gradient(to bottom, #606060, #ffffff)";
+    links.classList.add("open");
   }
 });
